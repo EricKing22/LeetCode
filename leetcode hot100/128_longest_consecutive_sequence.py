55318b044ad74c8ea3e41e232032b49194a174f1
Eric <54dashuaib@gmail.com>
2026-07-19 14:43:36 +0100

    Solutions Update

--- leetcode hot100/128_longest_consecutive_sequence.py
+++ leetcode hot100/128_longest_consecutive_sequence.py
@@ -2,32 +2,20 @@ from typing import List
 
 
 class Solution:
-    def __init__(self):
-        self.ans = 1
+
 
     def longestConsecutive(self, nums: List[int]) -> int:
-        nums.sort()
-        memory = [0]
+        nums = set(nums)
 
-        if nums == []:
-            return 0
+        ans = 0
 
-        def dfs(current_index, memory):
-            if current_index == len(nums) - 1:
-                return
+        for num in nums:
+            if num - 1 not in nums:
+                length = 1
 
-            if nums[current_index + 1] == nums[memory[-1]] + 1:
-                memory.append(current_index + 1)
-                self.ans = max(self.ans, len(memory))
-                dfs(current_index + 1, memory)
-                memory.pop()
-            elif nums[current_index + 1] == nums[memory[-1]]:
-                dfs(current_index + 1, memory)
-            else:
-                dfs(current_index + 1, [current_index + 1])
+                while num + length in nums:
+                    length += 1
 
-        dfs(0, memory)
+                ans = max(ans, length)
 
-        return self.ans
-
-
+        return ans
